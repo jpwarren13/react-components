cc33a79144ef6ef3bc6ad4922e8fe9f6b625a677
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-10-01 14:19:05 -0700

    Works with dynamically updating list

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+var App = () => (
+	<div>
+	<h2> My Todo List</h2>
+	<TodoList />
+	</div>
+
+);
+
+var GroceryListItem = (props) => (
+	<li>{props.item}</li>
+)
+
+var GroceryList = (props) => (
+	<ul>
+	{props.items.map((item) => (<GroceryListItem item={item} />))}
+	</ul>
+);
+
+
+
+ReactDOM.render(<GroceryList items={['kale', 'cucumber']} />,  document.getElementById('app'));
+
